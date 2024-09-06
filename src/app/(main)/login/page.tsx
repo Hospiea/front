1b638c3e4e6cb178284afce86786417f887ea61f
Hospiea <155417815+Hospiea@users.noreply.github.com>
2024-09-07 07:02:56 +0900

    qwe

--- src/app/(main)/login/page.tsx
+++ src/app/(main)/login/page.tsx
@@ -4,7 +4,7 @@ const Login = () => {
 
     return(
         <div className={styles.container}>
-
+            <h2>카카오 API 키 깃허브 PUBLIC노출로 인해 추후 구현 예정입니다 🥰</h2>
         </div>
     )
 }
---
+++ src/app/api/oauth/route.ts
@@ -0,0 +1,8 @@
+
+
+const GET = async () => {
+
+
+}
+
+export {GET};
