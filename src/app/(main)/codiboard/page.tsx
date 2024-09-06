d05e1e387312c5999138b8752f5e3c82c4b8f9d5
Hospiea <155417815+Hospiea@users.noreply.github.com>
2024-09-07 07:53:22 +0900

    Update page.tsx

--- src/app/(main)/codiboard/page.tsx
+++ src/app/(main)/codiboard/page.tsx
@@ -9,7 +9,7 @@ const Codiboard = () => {
             <h1>자신의 스타일을 자랑해주세요!</h1>
             <div className={styles.inner}>
                 <CodiContent src="/image.jpg" title="냥님의 코디" body="어제 백화점 직원한테 넘어가서 모자샀는데 잘 어울리나요? ㅜㅜ 별론 것 같아서요" />
-                <CodiContent src="/image2.jpeg" title="집냥님의 코디" body="저도 그저께 그 백화점 다녀왔어요 오늘 환불받으러 가요 눈빛연습 중이에요 환불확률 객관적으로 부탁드립니다?" />
+                <CodiContent src="/image2.jpeg" title="집냥님의 코디" body="저도 그저께 그 백화점 다녀왔어요 오늘 환불받으러 가요 눈빛연습 중이에요 환불확률 객관적으로 부탁드립니다" />
             </div>
         </div>
     )
