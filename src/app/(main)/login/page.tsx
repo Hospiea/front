d4a4e72912fece50fa4d85192b6968f79636bc08
Hospiea <155417815+Hospiea@users.noreply.github.com>
2024-09-07 07:50:42 +0900

    Update page.tsx

--- src/app/(main)/login/page.tsx
+++ src/app/(main)/login/page.tsx
@@ -4,7 +4,7 @@ const Login = () => {
 
     return(
         <div className={styles.container}>
-            <h2>카카오 API 키 깃허브 PUBLIC노출로 인해 추후 구현 예정입니다 🥰</h2>
+            <h2>카카오로그인은 API 키 깃허브 PUBLIC노출로 인해 추후 구현 예정입니다 🥰</h2>
         </div>
     )
 }
