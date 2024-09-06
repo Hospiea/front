7db2172a3cd22b600a849168830c38e9b168e8e4
Hospiea <155417815+Hospiea@users.noreply.github.com>
2024-09-07 06:35:52 +0900

    Update route.ts

--- src/app/api/codi/route.ts
+++ src/app/api/codi/route.ts
@@ -1,21 +1,21 @@
 import { NextRequest, NextResponse } from "next/server";
 import OpenAI from "openai";
 
-const APIKEY = process.env.NEXT_PUBLIC_AI_APIKEY;
-const openai = new OpenAI({apiKey: `${APIKEY}`});
+// const APIKEY = process.env.NEXT_PUBLIC_AI_APIKEY;
+// const openai = new OpenAI({apiKey: `${APIKEY}`});
 
 
 
-const GET = async (req:NextRequest) => {
-    const completion = await openai.chat.completions.create({
-        model: "gpt-3.5-turbo",
-        messages: [
-            {"role": "user", "content": "write a haiku about ai"}
-        ]
-    });
+// const GET = async (req:NextRequest) => {
+//     const completion = await openai.chat.completions.create({
+//         model: "gpt-3.5-turbo",
+//         messages: [
+//             {"role": "user", "content": "write a haiku about ai"}
+//         ]
+//     });
 
 
-    return NextResponse.json(completion);
-}
+//     return NextResponse.json(completion);
+// }
 
-export {GET}
+// export {GET}
