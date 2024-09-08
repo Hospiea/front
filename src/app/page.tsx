a8aca6975705e1f782cb66d08ecea92075757e73
Hospiea <155417815+Hospiea@users.noreply.github.com>
2024-09-09 08:24:01 +0900

    qwe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,8 @@
-'use client'
-import useOnStart from "@/hooks/useOnStart";
-import axios from 'axios';
+
 
 export default function Home() {
 
-  useOnStart(async ()=>{
-    const result = await axios.get("/api/comment");
-    console.log(result);
-  })
-
+  
   return (
     <div style={{display: "flex", justifyContent: "center", marginTop: "2em"}}>
       <h1>🥰 방문을 환영합니다 🥰</h1>
--- src/components/comment/comment.tsx
+++ src/components/comment/comment.tsx
@@ -1,4 +1,9 @@
+'use client'
+
 import styles from './style.module.css';
+import useOnStart from "@/hooks/useOnStart";
+import axios from 'axios';
+import { useState } from 'react';
 
 export interface CommentType {
     nickname: string;
@@ -9,13 +14,37 @@ export interface CommentType {
 
 const CommentComp = () => {
 
+    const [comments, setComments] = useState<CommentType[]>([]);
+
+    useOnStart(async ()=>{
+        const result = await axios.get("/api/comment");
+        const data: CommentType[] = result.data.map((item: CommentType) => {
+            const newData: CommentType = {
+                nickname: item.nickname,
+                text: item.text,
+                pw: item.pw,
+            };
+            return newData;
+        });
+
+        setComments(data);
+      })
+    
+
+
     return(
         <div className={styles.container}>
             <div className={styles.title}>
                 <h1>댓글 게시판(자유로운 의견 및 잘못된 부분 지적 환영입니다 ^^)</h1>
             </div>
-            <div>
-                
+            <div style={{marginTop: "0.2em"}}>
+                {comments && comments.map((item, index) => {
+                    return(
+                        <div key={index}>
+                            {item.nickname}님 : {item.text}
+                        </div>
+                    )
+                })}
             </div>
         </div>
     )
